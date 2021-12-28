2892fc581aa362307be57f4a930e3d49ad0bbdbb
Наталья Титаренко <96578594+Titarenko-Nata@users.noreply.github.com>
2021-12-28 12:45:48 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,40 +1,92 @@
+
 <?
+class Bankomat{
+	public $file = 'pin.txt';
+	public $balanceFile = 'balance.txt';
+	public $pin;
+	public $pinEnter;
+  public $balance;
+	public $action;
+	public $money;
 
-class Bankomat{ //создаем класс Банкомат
-	
+ public function __construct(){
+			$this->pin = file_get_contents($this->file);
+			$this->balance = file_get_contents($this->balanceFile);
+			$this->pinEnter = isset($_POST['PIN']) ? $_POST['PIN'] : null;
+			$this->action = isset($_POST['action']) ? $_POST['action'] : null;
+			$this->money = isset($_POST['SUMM']) ? $_POST['SUMM'] : null;
+	}
 
-	public function enter_money($money){ // создаем функцию выдачи суммы купюрами от большего наминала к меньшему
+ public function enterPin(){
+ 	
+		  	echo '<form method="POST">
+			 <input name = "PIN" type="text" placeholder="Введите пин"/>
+			 <input type="submit" name="submit" value="Отправить" />
+		   </form>';
 
-		$notes = array(1,2,5,10,20,50,100,200,500); // номиналы банкнот в банкомате
-         
-          if($money > 10000){
-        	$res = "Недостаточно денег в банкомате";
-        	
-           }else{
-           	echo "Cумма для выдачи: $money <br>";
-         	  for($i = count($notes)-1;($i >= 0 && $money!=0);$i--){  // идем от конца массива к началу
-		    	$value=$notes[$i];                                   // записываем элемент массива в переменную
-					if($money>=$value){                             // сравниваем запрашиваему сумму со значение переменной, если она больше или равна
-						$k = (int) ($money / $value);              // ищем результат деления  целое число
-						$cntNotes = min($k, $notes);              // количество банкнот каждого номинала
-						// указывает склонение слова купюра в зависимости от количества
-						if (($cntNotes==1) || (($cntNotes>20) && ($cntNotes%10 == 1))) {
-                          $resText = $cntNotes.' купюра';
-                        }else if ((($cntNotes>=2) && ($cntNotes<=4)) || (($cntNotes>20) && (($cntNotes%10==2) || ($cntNotes%10==3) || ($cntNotes%10==4)))) {
-                          $resText = $cntNotes.' купюры';
-                         }else{
-                            $resText = $cntNotes.' купюр';
-                         }
-							$money = $money - $cntNotes * $value;                                   // считаем остаток суммы
-							echo "Номинал $value грн. Выдано $resText. Остаток суммы = $money<br>"; // выводим результат на экран
-		            }
-	              }
-                }
-           return $res;
-        }
-    }
+  }
 
-$bank = new Bankomat(); // создаем объект
-echo $bank -> enter_money(5468); //вызываем функцию с переданным параметром суммы для выдачи
 
+ public function comparePin(){
+		if($this->pinEnter == $this->pin){
+			echo '<form method="POST">
+			<input type="submit" name="action" value="Баланс" />
+			<input type="submit" name="action" value="Снять" />
+			<input type="submit" name="action" value="Выход" />
+			</form>';
+											
+		}else{
+			echo "Пин-код введен не правильно";
+		}	 
+	}
+
+	public function action() {
+    if($this->action == "Баланс"){
+		 $this -> showBalance();
+			}else if($this->action == "Снять"){
+			  echo '<form method="post">
+				<input name = "SUMM" type="text" placeholder="Введите сумму для снятия"/>
+				<input type="submit" name="GET" value="Снять" />
+        </form>';
+			}else if($this->action == "Выход"){
+				 unset($this->pinEnter, $this->action, $this->money);
+		}
+	}
+
+  
+public function showBalance(){
+	echo "Ваш баланс: $this->balance";
+}
+
+public function updateBalance(){
+	$this->balance = $this->balance - $this->money;
+	file_put_contents($this->balanceFile, $this->balance);
+}
+	 
+
+ public function enterMoney(){
+   if($this->money > $this->balance){
+     echo "Недостаточно денег на балансе";
+      }else{
+       echo "Выдана сумма: $this->money";
+         $this->updateBalance();
+		  }   
+     }
+                 
+}
 ?>
+
+<?
+$myBank = new Bankomat();
+$myBank -> enterPin();
+ if(isset($myBank->pinEnter)) {
+ 	  $myBank -> comparePin();
+ 	}
+ if(isset($myBank->action)) {
+ 		$myBank -> action();
+ 	}
+ if(isset($myBank->money)) {
+ 		$myBank -> enterMoney();
+ 	}
+
+
